493f47cd220c76789453791d5157e3b322a07a64
Ceren Akman <42838160+bilgidisk@users.noreply.github.com>
2018-09-04 21:01:37 +0300

    Sql Procedure C#

---
+++ Procedure/Procedure/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data;
+using System.Data.SqlClient;
+using System.Data.Sql;
+
+namespace Procedure
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection("Data Source =.; Initial Catalog=numara;Integrated Security=true");
+            SqlCommand cmd = new SqlCommand("kisigetir", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            SqlDataAdapter dr = new SqlDataAdapter(cmd);
+            DataSet ds = new DataSet();
+            dr.Fill(ds, "Bilgi");
+            dataGridView1.DataSource = ds.Tables[0];
+        }
+    }
+}
